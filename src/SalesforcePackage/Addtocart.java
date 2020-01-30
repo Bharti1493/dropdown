9ba7c3c5b1808cc85f7757c479dade1c43e7a356
bharti1493 <thakurbharti123456@gmail.com>
2020-01-30 14:00:11 +0530

    add to Cart commit

--- src/SalesforcePackage/Addtocart.java
+++ src/SalesforcePackage/Addtocart.java
@@ -1,6 +1,8 @@
 package SalesforcePackage;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,22 +11,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Addtocart {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		int j=0;
 		System.setProperty("webdriver.chrome.driver","E:\\yashDoc\\Data\\WebDrivers\\Chrome\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		String [] vegies= {"Carrot","Tomato","Cauliflower"};
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-	 List<WebElement> product=driver.findElements(By.cssSelector("h4[class*='product-name']"));
+		Thread.sleep(2000);
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+	
+		String [] vegies= {"Carrot","Tomato","Cauliflower"};
+	 List<WebElement> product=driver.findElements(By.cssSelector(".product-name"));
 		
 		for(int i=0;i<product.size();i++)
 		{
-			String name=product.get(i).getText();
-			if(name.contains("vegies"))
+			String[] name=product.get(i).getText().split("-");
+			String Formatedname=name[0].trim();
+			List al= Arrays.asList(vegies);
+			if(al.contains(Formatedname))
 			{
-				driver.findElement(By.cssSelector("//button[@type='button']")).click();
+				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				Thread.sleep(2000);
+				if(j==3)
+				{
+					Systm.out.println("Stop loop")
+				}
+				
 			}
+			
+			
 		}
+		driver.findElement(By.xpath("//img[@alt='Cart']")).click();
 
 	}
-
 }
+
+
